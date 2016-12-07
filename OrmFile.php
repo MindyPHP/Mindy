fa42b911a61e5db75681fbc1ed709981e86a3aaa
Maksim Falaleev <max@studio107.ru>
2016-12-07 23:00:56 +0300

    Issue #7

--- OrmFile.php
+++ OrmFile.php
@@ -9,18 +9,36 @@
 namespace Mindy\Orm;
 
 use League\Flysystem\FilesystemInterface;
+use Mindy\Application\App;
 
+/**
+ * Class OrmFile
+ * @package Mindy\Orm
+ */
 class OrmFile
 {
+    /**
+     * @var FilesystemInterface|null
+     */
     protected static $filesystem;
 
+    /**
+     * @param FilesystemInterface $filesystem
+     */
     public static function setFilesystem(FilesystemInterface $filesystem)
     {
         self::$filesystem = $filesystem;
     }
 
+    /**
+     * @return FilesystemInterface|null
+     */
     public static function getFilesystem()
     {
+        if (null === self::$filesystem) {
+            // TODO https://github.com/MindyPHP/Mindy/issues/7
+            self::$filesystem = App::getInstance()->getComponent('oneup_flysystem.default_filesystem');
+        }
         return self::$filesystem;
     }
 }
