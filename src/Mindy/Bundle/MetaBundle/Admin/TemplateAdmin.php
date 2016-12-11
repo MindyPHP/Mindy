3709a23305d214b3728d53660aff038d4db59f09
Maksim Falaleev <max@studio107.ru>
2016-12-12 00:18:24 +0300

    Added meta template admin and form

---
+++ src/Mindy/Bundle/MetaBundle/Admin/TemplateAdmin.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: max
+ * Date: 12/12/2016
+ * Time: 00:15
+ */
+
+namespace Mindy\Bundle\MetaBundle\Admin;
+
+use Mindy\Bundle\MetaBundle\Form\TemplateForm;
+use Mindy\Bundle\MetaBundle\Model\Template;
+use Mindy\Bundle\MindyBundle\Admin\AbstractModelAdmin;
+
+class TemplateAdmin extends AbstractModelAdmin
+{
+    public $columns = ['code'];
+
+    public function getFormType()
+    {
+        return TemplateForm::class;
+    }
+
+    public function getModelClass()
+    {
+        return Template::class;
+    }
+}
---
+++ src/Mindy/Bundle/MetaBundle/Form/TemplateForm.php
@@ -0,0 +1,48 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: max
+ * Date: 12/12/2016
+ * Time: 00:16
+ */
+
+namespace Mindy\Bundle\MetaBundle\Form;
+
+use Mindy\Bundle\MetaBundle\Model\Template;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints as Assert;
+
+class TemplateForm extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('code', TextType::class, [
+                'label' => 'Код шаблона',
+                'constraints' => [
+                    new Assert\NotBlank
+                ]
+            ])
+            ->add('content', TextareaType::class, [
+                'label' => 'Шаблон',
+                'constraints' => [
+                    new Assert\NotBlank
+                ]
+            ])
+            ->add('submit', SubmitType::class, [
+                'label' => 'Сохранить'
+            ]);
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Template::class
+        ]);
+    }
+}
