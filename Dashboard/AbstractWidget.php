e57dcf77d4c7541a0c50fd378354673a29189b30
Maksim Falaleev <max@studio107.ru>
2016-12-12 21:50:28 +0300

    Split MindyBundle into AdminBundle, FileBundle, DashboardBundle

---
+++ Dashboard/AbstractWidget.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: max
+ * Date: 14/11/2016
+ * Time: 20:33
+ */
+
+namespace Mindy\Bundle\DashboardBundle\Dashboard;
+
+abstract class AbstractWidget implements WidgetInterface
+{
+
+}
---
+++ DashboardBundle.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: max
+ * Date: 12/12/2016
+ * Time: 20:50
+ */
+
+namespace Mindy\Bundle\DashboardBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class DashboardBundle extends Bundle
+{
+
+}
