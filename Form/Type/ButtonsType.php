f98c7a9dc00b468e7f45efe68926ba08fa485319
Falaleev Maxim <max@studio107.ru>
2017-01-24 17:47:06 +0300

    update ui

--- Form/Type/ButtonsType.php
+++ Form/Type/ButtonsType.php
@@ -1,9 +1,11 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: max
- * Date: 30/11/2016
- * Time: 21:05.
+
+/*
+ * (c) Studio107 <mail@studio107.ru> http://studio107.ru
+ * For the full copyright and license information, please view
+ * the LICENSE file that was distributed with this source code.
+ *
+ * Author: Maxim Falaleev <max@studio107.ru>
  */
 
 namespace Mindy\Bundle\AdminBundle\Form\Type;
@@ -20,12 +22,15 @@ class ButtonsType extends AbstractType
         $builder
             ->add('save', SubmitType::class, [
                 'label' => 'Сохранить',
+                'attr' => ['class' => 'b-button'],
             ])
             ->add('save_and_return', SubmitType::class, [
                 'label' => 'Сохранить и вернуться',
+                'attr' => ['class' => 'b-button'],
             ])
             ->add('save_and_create', SubmitType::class, [
                 'label' => 'Сохранить и создать',
+                'attr' => ['class' => 'b-button'],
             ]);
     }
 
