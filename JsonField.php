9a04c695a967667534de052a0646dbc53e04c18c
Maksim Falaleev <max@studio107.ru>
2017-02-01 20:48:59 +0300

    php-cs-fixer

--- JsonField.php
+++ JsonField.php
@@ -1,5 +1,13 @@
 <?php
 
+/*
+ * (c) Studio107 <mail@studio107.ru> http://studio107.ru
+ * For the full copyright and license information, please view
+ * the LICENSE file that was distributed with this source code.
+ *
+ * Author: Maxim Falaleev <max@studio107.ru>
+ */
+
 namespace Mindy\Orm\Fields;
 
 use Doctrine\DBAL\Platforms\AbstractPlatform;
@@ -45,9 +53,9 @@ class JsonField extends TextField
     {
         if (is_string($value)) {
             return parent::convertToPHPValue($value, $platform);
-        } else {
-            return $value;
         }
+
+        return $value;
     }
 
     /**
