988c9c1e8ce2d9ee8ce23559489862cceaeb7bc7
Falaleev Maxim <max@studio107.ru>
2017-02-07 10:09:13 +0300

    raw upload

--- src/Mindy/Bundle/MenuBundle/Form/MenuForm.php
+++ src/Mindy/Bundle/MenuBundle/Form/MenuForm.php
@@ -10,8 +10,10 @@
 
 namespace Mindy\Bundle\MenuBundle\Form;
 
+use Mindy\Bundle\AdminBundle\Form\Type\ButtonsType;
 use Mindy\Bundle\MenuBundle\Model\Menu;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -19,7 +21,20 @@ class MenuForm extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
-        // TODO
+        $builder
+            ->add('name', TextType::class, [
+                'label' => 'Название'
+            ])
+            ->add('slug', TextType::class, [
+                'label' => 'Слаг',
+                'required' => false
+            ])
+            ->add('url', TextType::class, [
+                'label' => 'Адрес',
+                'required' => false,
+                'help' => 'Ссылка для перехода'
+            ])
+            ->add('buttons', ButtonsType::class);
     }
 
     public function configureOptions(OptionsResolver $resolver)
