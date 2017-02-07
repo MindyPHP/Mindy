4f57b22fbef0c4ae9035a9c2e2811def93f1e640
Falaleev Maxim <max@studio107.ru>
2017-02-07 17:37:59 +0300

    refact meta

---
+++ Traits/AbsoluteUrlInterface.php
@@ -0,0 +1,20 @@
+<?php
+
+/*
+ * (c) Studio107 <mail@studio107.ru> http://studio107.ru
+ * For the full copyright and license information, please view
+ * the LICENSE file that was distributed with this source code.
+ */
+
+namespace Mindy\Bundle\MindyBundle\Traits;
+
+/**
+ * Interface AbsoluteUrlInterface
+ */
+interface AbsoluteUrlInterface
+{
+    /**
+     * @return string
+     */
+    public function getAbsoluteUrl();
+}
