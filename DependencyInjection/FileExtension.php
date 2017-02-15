3852b44d98dc9f6b5fdba40eae0752c09acc2fe1
Maksim Falaleev <max@studio107.ru>
2017-02-15 19:48:17 +0300

    remove dependency frim imageine bundle

--- DependencyInjection/FileExtension.php
+++ DependencyInjection/FileExtension.php
@@ -13,7 +13,7 @@ namespace Mindy\Bundle\FileBundle\DependencyInjection;
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Extension\Extension;
-use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
+use Symfony\Component\DependencyInjection\Loader\XmlFileLoader;
 
 class FileExtension extends Extension
 {
@@ -27,7 +27,7 @@ class FileExtension extends Extension
      */
     public function load(array $configs, ContainerBuilder $container)
     {
-        $loader = new YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
-        $loader->load('services.yml');
+        $loader = new XmlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
+        $loader->load('services.xml');
     }
 }
