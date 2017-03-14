7ffb5a71be7e3837e772428ea3e2102b9d4b997f
Falaleev Maxim <max@studio107.ru>
2017-03-14 14:14:36 +0300

    subtree up

---
+++ src/Mindy/Bundle/FileBundle/FileBundle.php
@@ -0,0 +1,23 @@
+<?php
+
+/*
+ * This file is part of Mindy Framework.
+ * (c) 2017 Maxim Falaleev
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Mindy\Bundle\FileBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+/**
+ * Created by PhpStorm.
+ * User: max
+ * Date: 12/12/2016
+ * Time: 20:59.
+ */
+class FileBundle extends Bundle
+{
+}
