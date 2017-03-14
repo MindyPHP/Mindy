fbb59f1e0a73a6bf33822f4beccab9c91da7b871
Maksim Falaleev <max@studio107.ru>
2017-03-14 21:25:52 +0300

    Fix pagination

--- src/Mindy/Pagination/Tests/PaginationTest.php
+++ src/Mindy/Pagination/Tests/PaginationTest.php
@@ -1,11 +1,11 @@
 <?php
 
 /*
- * (c) Studio107 <mail@studio107.ru> http://studio107.ru
- * For the full copyright and license information, please view
- * the LICENSE file that was distributed with this source code.
+ * This file is part of Mindy Framework.
+ * (c) 2017 Maxim Falaleev
  *
- * Author: Maxim Falaleev <max@studio107.ru>
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
  */
 
 namespace Mindy\Pagination\Tests;
@@ -159,10 +159,10 @@ class PaginationTest extends \PHPUnit_Framework_TestCase
     public function testWrongPage()
     {
         $pager = $this->createPagination(range(1, 20), [
-            'page' => 2,
+            'page' => 20,
             'pageSize' => 30,
         ]);
-        $this->assertSame(2, $pager->getPage());
+        $this->assertSame(20, $pager->getPage());
         $this->assertSame(30, $pager->getPageSize());
 
         $data = $pager->paginate();
