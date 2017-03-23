862182f2b52de0d74ac017293155ee74ae794962
Maksim Falaleev <max@studio107.ru>
2017-03-23 21:18:38 +0300

    Added ConnectionAwareInterface and trait, refact connection part qs and manager

---
+++ src/Mindy/QueryBuilder/ConnectionAwareTrait.php
@@ -0,0 +1,41 @@
+<?php
+
+/*
+ * This file is part of Mindy Framework.
+ * (c) 2017 Maxim Falaleev
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Mindy\QueryBuilder;
+
+use Doctrine\DBAL\Connection;
+
+trait ConnectionAwareTrait
+{
+    /**
+     * @var Connection
+     */
+    protected $connection;
+
+    /**
+     * @param Connection $connection
+     *
+     * @return $this
+     */
+    public function setConnection(Connection $connection)
+    {
+        $this->connection = $connection;
+
+        return $this;
+    }
+
+    /**
+     * @return Connection
+     */
+    public function getConnection()
+    {
+        return $this->connection;
+    }
+}
