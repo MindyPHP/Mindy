e75e2f982a48121df39e34ff6963f8826edd6103
Falaleev Maxim <max@studio107.ru>
2017-07-21 12:22:26 +0300

    i18n, docblocks

--- Model/Seo.php
+++ Model/Seo.php
@@ -1,9 +1,11 @@
 <?php
 
 /*
- * (c) Studio107 <mail@studio107.ru> http://studio107.ru
- * For the full copyright and license information, please view
- * the LICENSE file that was distributed with this source code.
+ * This file is part of Mindy Framework.
+ * (c) 2017 Maxim Falaleev
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
  */
 
 namespace Mindy\Bundle\SeoBundle\Model;
@@ -17,12 +19,13 @@ use Symfony\Component\Validator\Constraints as Assert;
 /**
  * Class Meta.
  *
- * @property string $domain
+ * @property string $host
  * @property string $title
  * @property string $url
  * @property string $keywords
  * @property string $canonical
  * @property string $description
+ * @property int|bool $is_auto
  */
 class Seo extends Model
 {
